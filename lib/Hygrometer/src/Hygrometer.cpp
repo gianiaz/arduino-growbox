09cee3d0a95b7d678116eb8bedf5389b532c49a7
Giovanni Lenoci <giovanni.lenoci@facile.it>
2018-11-13 23:21:35 +0100

    Adds Hygrometer, fixes timing issues

---
+++ lib/Hygrometer/src/Hygrometer.cpp
@@ -0,0 +1,38 @@
+#include "Arduino.h"
+#include "Hygrometer.h"
+#include "Time.h"
+
+
+Hygrometer::Hygrometer(uint8_t pin, int secondsDelayBetwenLectures)
+{
+  _pin = pin;
+  _milliseconds_delay_betwen_lectures = secondsDelayBetwenLectures * 1000;
+  _last_lecture_time = 0;
+  _last_lecture = -1;
+}
+
+void Hygrometer::setUp(bool debug) {
+  _debug = debug;
+}
+
+int Hygrometer::getHumidity() {
+  return _last_lecture;
+}
+
+void Hygrometer::updateStatus() {
+
+  if(millis() > _milliseconds_delay_betwen_lectures && (millis() - _milliseconds_delay_betwen_lectures) > _last_lecture_time) {
+    _last_lecture_time = millis();
+    if(_debug) {
+      Serial.println("Reading...");
+    }
+    _last_lecture = analogRead(_pin);		//Read analog value
+    _last_lecture = constrain(_last_lecture,400,1023);	//Keep the ranges!
+  	_last_lecture = map(_last_lecture,400,1023,100,0);	//Map value : 400 will be 100 and 1023 will be 0
+    if(_debug) {
+      Serial.print("Soil humidity: ");
+    	Serial.print(_last_lecture);
+    	Serial.println("%");
+    }
+  }
+}
---
+++ lib/Hygrometer/src/Hygrometer.h
@@ -0,0 +1,21 @@
+#include "Arduino.h"
+#include "Time.h"
+#ifndef Hygrometer_h
+#define Hygrometer_h
+
+class Hygrometer
+{
+  public:
+    Hygrometer(uint8_t pin, int secondsDelayBetwenLectures);
+    void updateStatus();
+    void setUp(bool debug);
+    int getHumidity();
+  private:
+    int _debug;
+    uint8_t _pin;
+    unsigned long _milliseconds_delay_betwen_lectures;
+    unsigned long _last_lecture;
+    unsigned long _last_lecture_time;
+};
+
+#endif
